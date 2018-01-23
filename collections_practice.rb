865bb821128659212e6fdabec18101912f7c1f1e
Kyle Harris <k_harris95@live.com>
2018-01-23 13:51:49 +0000

    automatically backed up by learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,32 @@
+def sort_array_asc(array)
+  return array.sort
 
+end
+
+def sort_array_desc(array)
+  return array.sort { |a, b| b <=> a }
+end
+
+def SORT_ARRAY_CHAR_COUNT(array)
+
+end
+
+def SWAP_ELEMENTS(array)
+
+end
+
+def KESHA_MAKER(array)
+
+end
+
+def FIND_A(array)
+
+end
+
+def SUM_ARRAY(array)
+
+end
+
+def ADD_S(array)
+
+end
